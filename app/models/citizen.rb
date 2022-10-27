4c53fddfa58d6921c823bf82a866bf33af406861
ArcelinoNeto <arcelino.neto@gmail.com>
2022-10-27 15:06:48 -0300

    Correção na associação Citizen com Address

--- app/models/citizen.rb
+++ app/models/citizen.rb
@@ -1,7 +1,7 @@
 class Citizen < ApplicationRecord
   enum status: [:active, :inactive]
   
-  has_one :address
+  has_one :address, :dependent => :destroy
   belongs_to :county
   
   accepts_nested_attributes_for :address
